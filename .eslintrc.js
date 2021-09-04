0c6bcc6948a373e68041ba37266edf3bb3ca8d72
cdisanto <christian.disanto@enhancers.it>
2021-09-04 15:29:12 +0200

    added eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  endOfLine: 'auto',
+  plugins: ['react-hooks'],
   rules: {
+    'prettier/prettier': 'off',
+    'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-key': 'warn',
+    'no-inline-styles': 'off',
+    'react/no-deprecated': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 };
