be085b991e8ada8dd47eab550df0faf3ebf8cab5
cdisanto <christian.disanto@enhancers.it>
2021-09-13 22:37:04 +0200

    added home scroll function 2

--- src/screens/home/index.ts
+++ src/screens/home/index.ts
@@ -1,6 +1,8 @@
 import Home from './Home';
+import * as navigationActions from '../../store/modules/navigation/navigation.action';
 import * as postsActions from '../../store/modules/posts/posts.action';
 import * as postsSelector from '../../store/modules/posts/posts.selector';
+import * as navigationSelector from '../../store/modules/navigation/navigation.selector';
 import {Dispatch} from 'redux';
 import {connect} from 'react-redux';
 
@@ -8,11 +10,15 @@ const mapStateToProps = (state: any) => ({
   userPosts: postsSelector.retrieveUserPosts(state),
   contentColor: postsSelector.retrieveContentColor(state),
   isLoading: postsSelector.retrievePostsLoader(state),
+  isHiddenHeader: navigationSelector.retrieveIsHiddenHeader(state),
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   getHomePosts: () => {
     dispatch(postsActions.getHomePosts() as any);
+  },
+  setHiddenHeader: (payload: any) => {
+    dispatch(navigationActions.setHiddenHeader(payload) as any);
   },
 });
 
