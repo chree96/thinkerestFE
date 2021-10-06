41b4ecf7a51c2778c3458824bd0005d1446b7214
cdisanto <christian.disanto@enhancers.it>
2021-10-06 21:14:56 +0200

    added content types

---
+++ src/types/content.ts
@@ -0,0 +1,11 @@
+export enum ContentSearchSections {
+  TOP_RATED = 'TOP_RATED',
+  RECOMMENDED = 'RECOMMENDED',
+  GENRE = 'GENRE',
+}
+
+export enum ContentGenre {
+  ACTION = 'ACTION',
+  ROMANTIC = 'ROMANTIC',
+  ADVENTURE = 'ADVENTURE',
+}
