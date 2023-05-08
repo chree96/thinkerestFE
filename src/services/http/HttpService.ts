53d49d60438ead8e430465dded52cd0608dc085d
cdisanto <christian.disanto@enhancers.it>
2023-05-08 23:38:37 +0200

    added new http address

--- src/services/http/HttpService.ts
+++ src/services/http/HttpService.ts
@@ -1,2 +1,2 @@
-export const HttpAddress = 'http://10.0.2.2:3030/';
-// export const HttpAddress = 'http://192.168.1.254:3030/';
+// export const HttpAddress = 'http://10.0.2.2:3030/'; //ONLY FOR DEBUGGING
+export const HttpAddress = 'http://192.168.1.54:3030/'; //ONLY FOR REAL DEVICE
